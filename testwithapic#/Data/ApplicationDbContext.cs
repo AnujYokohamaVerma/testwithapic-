e4519b89954531e081dbd0671ad1210df6977e4b
AnujV <anujvermayokohamatire@gmail.com>
2024-09-27 12:52:52 -0500

    new update done with some routing#

---
+++ testwithapic#/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace testwithapic_.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options) 
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=YourDatabaseName;Trusted_Connection=True;");
+            }
+
+        }
+    }
+}
