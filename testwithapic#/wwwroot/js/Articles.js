5a8852e08ee353720c85d470c1b8781f489ead78
AnujV <anujvermayokohamatire@gmail.com>
2024-10-14 12:41:37 -0500

    got all the views added back in

--- testwithapic#/wwwroot/js/Articles.js
+++ testwithapic#/wwwroot/js/Articles.js
@@ -9,8 +9,31 @@ function loadDataTable() {
         "columns": [
             { data: 'title', "width": "15%" },
             { data: 'summary', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="/Jinchuriki_1/Articles/Read?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil"></i> Read</a>
+
+                    </div >`
+
+                },
+                "width": "15"
+            },
             { data: 'createdDate', "width": "15%" },
-            { data: 'modifiedDate', "width": "15%" }
+            { data: 'modifiedDate', "width": "25%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="/Jinchuriki_1/Articles/Edit?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil"></i> Edit</a>
+                        <a href="/Jinchuriki_1/Articles/Delete?id=${data}" class="btn btn-danger mx-2"><i class="bi bi-trash"></i> Delete</a>
+
+                    </div >`
+                    
+                },
+                "width": "15"
+            }
         ]
     });
 }
